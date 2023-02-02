b1a9a60db78cd67448c9818a5f09af0bd645fb7e
Mateusz Gula <mateuszgua@wp.eu>
2023-02-02 10:29:50 +0100

    Feat: add main loop, add test picture, add file with colors

--- main.py
+++ main.py
@@ -40,3 +40,22 @@ def draw_function(event, x, y, flags, param):
 
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_function)
+
+while (True):
+
+    cv2.imshow("image", img)
+    if clicked:
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+        text = get_color_name(r, g, b) + ' R=' + str(r) + \
+            ' G=' + str(g) + ' B=' + str(b)
+        cv2.putText(img, text, (50, 50), 2, 0.8,
+                    (255, 255, 255), 2, cv2.LINE_AA)
+
+        if (r + g + b >= 600):
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+        clicked = False
+
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
