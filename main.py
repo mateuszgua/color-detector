59e4d6580ea1b58de665c70df893b43876b17bd8
Mateusz Gula <mateuszgua@wp.eu>
2023-02-02 10:09:09 +0100

    Feat: add function for get color from csv

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ r = g = b = xpos = ypos = 0
 
 index = ["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv("colors.csv", names=index, header=None)
+
+
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G -
+                                                int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if (d <= minimum):
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
